b1702706c1536f207da5aed532b39a5bab8a5c82
naldin <ronaldo.rrj@gmail.com>
2018-10-14 11:01:22 -0300

    Added handbreak

--- Clutch_Analog/Clutch_Analog.ino
+++ Clutch_Analog/Clutch_Analog.ino
@@ -1,12 +1,12 @@
 /*
-  Arduino code for clutch
-  to Logitech DFGT wheel
+  Arduino code for clutch and 
+  handbrake to Logitech DFGT wheel
   Potentiometer version
 
   For Arduino Leonardo/Micro
   Ronaldo Rezende Jr (@naldin)
   ronaldo.rrj at gmail.com
-  07/2018
+  09/2018
   https://github.com/naldin/DFGT_Clutch
   http://xyzturbo.blogspot.com
 
@@ -23,10 +23,12 @@
 #include "Joystick.h"
 
 //variable
-int val;
+int val;   //clutch
+int val2; //handbrake
 
 //constants
-const int analogIn = A0;
+const int analogIn = A0; //clutch
+const int analogIn2 = A1;//handbrake
 const int minPot = 235;
 const int maxPot = 360;
 
@@ -35,12 +37,18 @@ void setup() {
 }
 
 void loop() {
+
+  //clutch
   val = analogRead(analogIn); //read analog value
   val = map(val, minPot, maxPot, 0, 255); //mapping value between min and max pot value
   val = constrain(val, 0, 255); // Constrains min and max range between 0 and 255
 
+  //handbrake
+  val2 = analogRead(analogIn2);
+  val2 = map(val2, 0, 1023, 0, 255);
+
   // use the desired option
-  Joystick.setThrottle(val);
-  //Joystick.setZAxis(val);
-  //Joystick.setRudder(val);
+  Joystick.setThrottle(val); //clutch
+  //Joystick.setZAxis(val2);
+  Joystick.setRudder(val2);  //handbrake
 }
